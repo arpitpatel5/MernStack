139528da44ad288f5cb080b59408055cc44a86e0
arpitpatel5 <arpit.patel5005@gmail.com>
2024-07-07 19:44:48 +0530

    while and do-while loop

---
+++ 15_Iterations2.js
@@ -0,0 +1,26 @@
+//while and do-while
+
+//while
+let index =0
+while (index <= 10){
+console.log(`Value of index is ${index}`);
+index =index +2
+}
+
+
+//while loop using arrays
+let myArray = ['batman', "Superman",'Flash']
+let arr = 0
+while (arr<myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr =arr+1
+}
+
+
+//dowhile loop - work first, later condition check 
+ let score = 1 
+ do{
+    console.log(`Score is ${score}`);
+    score++
+ } while(score <= 10);
+
